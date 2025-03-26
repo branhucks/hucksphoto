a750d0b66efa5e1b23c784a07d2f3b0684a32a8b
Brandon Hucks <branhucks@gmail.com>
2025-03-26 17:53:31 -0400

    Changes necessary for successful deployment

--- hucksphoto/settings.py
+++ hucksphoto/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-lua5*k(*)ky-6s8k-qogwp+@@xu4^2ienfl5ip8v!^&goyy0ca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
